12a33a982a6739f456d9e677e8ac4bf20b5905e4
Rafael Gomez Blanes <gomezbl@gmail.com>
2022-01-21 21:44:27 +0100

    component entry point module is optional

--- core/componentloader.js
+++ core/componentloader.js
@@ -6,20 +6,40 @@
 "use strict";
 
 const Path = require("path");
+const fs = require("fs");
+
 const CoreConstants = global.gimport("coreconstants");
+const MantraUtils = global.gimport("mantrautils");
 
 module.exports = {
     /*
      * Loads a component given its full path folder
      * Folder name containing the component is the name of the component
      * /home/user/project/components/seo folder includes "seo component", so
-     * a file seo.js should exists containing the component module
+     * a file seo.js should exists containing the component module.
+     * 
+     * If it doesn't exists, then an anonymous instance of the object expected to export
+     * the component is created.
      */
     loadComponent: ( fullPathToComponent ) => {
-        return {
-            pathToComponent: fullPathToComponent,
-            config: require( Path.join( fullPathToComponent, CoreConstants.COMPONENTS_CONFIGFILENAME ) ),
-            component: require( Path.join( fullPathToComponent, Path.basename(fullPathToComponent) ) )()
-        };
+        const pathToEntryComponentFile = Path.join( fullPathToComponent, Path.basename(fullPathToComponent) ) + ".js";
+
+        if ( fs.existsSync( pathToEntryComponentFile ) ) {
+            return {
+                pathToComponent: fullPathToComponent,
+                config: require( Path.join( fullPathToComponent, CoreConstants.COMPONENTS_CONFIGFILENAME ) ),
+                component: require( pathToEntryComponentFile )()
+            };
+        } else {
+            return {
+                pathToComponent: fullPathToComponent,
+                config: require( Path.join( fullPathToComponent, CoreConstants.COMPONENTS_CONFIGFILENAME ) ),
+                component: {
+                    Start: {
+                        onStart: async () => {}
+                    }
+                }
+            };
+        }
     }    
 }
