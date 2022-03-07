a4aafb3f5dd22afb2a6187b1137451e4197a6250
Rafael Gomez Blanes <gomezbl@gmail.com>
2022-03-07 10:19:22 +0100

    If a prerequest doesn't exist, an error message is shown

--- core/lib/prerequests.js
+++ core/lib/prerequests.js
@@ -5,6 +5,8 @@
 
 "use strict";
 
+const MantraConsole = global.gimport("mantraconsole");
+
 module.exports = {
     /*
      * Check prerequests for a component name and command
@@ -12,8 +14,8 @@ module.exports = {
      * If false, then according to the prequest, a redirect will be preformed
      */
     async checkPR(componentName, command, req, res ) {
-        let resource;
         const bootstrap = global.Mantra.Bootstrap;
+        let resource;
 
         if ( bootstrap.ExistsView( componentName, command) ) {
             resource = bootstrap.GetView( componentName, command );
@@ -21,20 +23,22 @@ module.exports = {
             resource = bootstrap.GetGet( componentName, command );
         } else return true;
 
-
         if ( resource && typeof resource.PreRequest != 'undefined' ) {
             for( const prName of resource.PreRequest ) {
                 const preRequest = bootstrap.GetPreRequest(prName);
                 
-                if ( !preRequest ) throw Error(`Unable to locate PreRequest ${prName} for path ${req.path}`);
-
-                if ( !(await preRequest.Handler( res.MantraAPI, req )) ) {
-                    if ( preRequest.OnCancel ) {
-                        await preRequest.OnCancel(res.MantraAPI);
-                    } else {
-                        res.redirect("/");
+                if ( preRequest ) {
+                    if ( !(await preRequest.Handler( res.MantraAPI, req )) ) {
+                        if ( preRequest.OnCancel ) {
+                            await preRequest.OnCancel(res.MantraAPI);
+                        } else {
+                            res.redirect("/");
+                        }
+    
+                        return false;
                     }
-
+                } else {
+                    MantraConsole.error( `Unable to call no existing prerequest of name '${prName}'`);
                     return false;
                 }
             }
